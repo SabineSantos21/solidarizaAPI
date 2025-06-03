bc5d623ec62917866684fafe31546b9fa2a47457
Sabine Freiman <sabine.freiman@isolutions.com.br>
2025-06-03 20:47:11 -0300

    Testes User Controller

--- Solidariza/Controllers/UserController.cs
+++ Solidariza/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Solidariza.Services;
 using Microsoft.AspNetCore.Mvc;
 using Solidariza.Models.Enum;
 using Solidariza.Common;
+using Solidariza.Interfaces.Services;
 
 namespace Solidariza.Controllers
 {
@@ -10,19 +11,27 @@ namespace Solidariza.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly ConnectionDB _dbContext;
+        private readonly IUserService _userService;
+        private readonly IProfileService _profileService;
+        private readonly IValidateOrganizationService _validateOrganizationService;
+        private readonly IOrganizationInfoService _organizationInfoService;
 
-        public UserController(ConnectionDB dbContext)
+        public UserController(
+            IUserService userService,
+            IProfileService profileService,
+            IValidateOrganizationService validateOrganizationService,
+            IOrganizationInfoService organizationInfoService)
         {
-            _dbContext = dbContext;
+            _userService = userService;
+            _profileService = profileService;
+            _validateOrganizationService = validateOrganizationService;
+            _organizationInfoService = organizationInfoService;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUserById(int id)
         {
-            UserService userService = new UserService(_dbContext);
-
-            User? user = await userService.GetUserById(id);
+            User? user = await _userService.GetUserById(id);
 
             if (user == null)
             {
@@ -32,21 +41,19 @@ namespace Solidariza.Controllers
             return user;
         }
 
-        [HttpPost("")]
-        public async Task<ActionResult> PostUser(NewUser newUser)
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(NewUser newUser)
         {
             try
             {
-                UserService usuarioService = new UserService(_dbContext);
-
                 if (string.IsNullOrEmpty(newUser.Email))
                 {
                     return BadRequest("O e-mail é obrigatório.");
                 }
 
-                User? userVerify = await usuarioService.GetUserByEmail(newUser.Email);
-                
-                if(userVerify != null)
+                User? userVerify = await _userService.GetUserByEmail(newUser.Email);
+
+                if (userVerify != null)
                 {
                     return BadRequest("Este e-mail não está mais disponível para cadastro. Por favor, utilize outro e-mail ou, se já possui um cadastro, recupere o acesso ao anterior.");
                 }
@@ -58,42 +65,41 @@ namespace Solidariza.Controllers
 
                 newUser.Password = PasswordHash.HashPassword(newUser.Password);
 
-                User user = await usuarioService.CreateUser(newUser);
+                User user = await _userService.CreateUser(newUser);
 
-                NewProfile newProfile = new NewProfile()
+                NewProfile newProfile = new NewProfile
                 {
                     UserId = user.UserId,
                     Name = user.Name
                 };
 
-                ProfileService profileService = new ProfileService(_dbContext);
-                await profileService.CreateProfile(newProfile);
+                await _profileService.CreateProfile(newProfile);
 
                 if (user.Type == UserType.Organization)
                 {
-                    if (user.DocumentNumber == null) return BadRequest("Número do documento é obrigatório");
+                    if (string.IsNullOrEmpty(user.DocumentNumber))
+                    {
+                        return BadRequest("Número do documento é obrigatório para organizações.");
+                    }
 
-                    ValidateOrganizationService validateOrganizationService = new ValidateOrganizationService();
-                    ConsultCnpjResponse organizationValid = await validateOrganizationService.ConsultCNPJ(user.DocumentNumber);
+                    ConsultCnpjResponse organizationValid = await _validateOrganizationService.ConsultCNPJ(user.DocumentNumber);
 
-                    NewOrganizationInfoCNPJValid newOrganizationInfo = new NewOrganizationInfoCNPJValid()
+                    NewOrganizationInfoCNPJValid newOrganizationInfo = new NewOrganizationInfoCNPJValid
                     {
                         UserId = user.UserId,
                         IsOrganizationApproved = organizationValid.IsValid,
-                        DisapprovalReason = organizationValid.DisapprovalReason,
+                        DisapprovalReason = organizationValid.DisapprovalReason
                     };
 
-                    OrganizationInfoService organizationInfoService = new OrganizationInfoService(_dbContext); 
-                    await organizationInfoService.CreateOrganizationInfoCPNJValid(newOrganizationInfo);
+                    await _organizationInfoService.CreateOrganizationInfoCPNJValid(newOrganizationInfo);
                 }
 
                 return Ok(user);
             }
             catch (Exception ex)
-            {   
+            {
                 return Problem(ex.Message);
             }
-            
         }
     }
 }
