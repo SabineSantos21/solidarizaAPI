49ecef22aed3c5ed25cf2fb17ee8d41456da9d5c
Sabine Freiman <sabine.freiman@isolutions.com.br>
2025-06-03 20:19:25 -0300

    Testes ProfileController

--- Solidariza.Tests/ProfileControllerTest.cs
+++ Solidariza.Tests/ProfileControllerTest.cs
@@ -1,26 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Xunit;
+using Moq;
 using Solidariza.Controllers;
 using Solidariza.Models;
-using Solidariza.Models.Enum;
+using Solidariza.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
-using Solidariza.Services;
+using Solidariza.Models.Enum;
+using System.Linq;
 
 namespace Solidariza.Tests
 {
     public class ProfileControllerTests
     {
-        private readonly ProfileController _controller;
         private readonly ConnectionDB _dbContext;
+        private readonly ProfileController _controller;
 
         public ProfileControllerTests()
         {
             var options = new DbContextOptionsBuilder<ConnectionDB>()
-               .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-               .Options;
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
 
             _dbContext = new ConnectionDB(options);
 
@@ -58,44 +60,32 @@ namespace Solidariza.Tests
             {
                 ProfileId = 1,
                 Name = "Org Profile",
-                Phone = "123456789",
+                Phone = "1234567890",
                 Description = "Org Desc",
-                Address = "Org Address",
+                Address = "123 Org Street",
                 City = "Org City",
-                State = "Org State",
-                Zip = "00000-000",
-                UserId = userOrg.UserId
+                State = "OS",
+                Zip = "12345-678",
+                UserId = userOrg.UserId,
+                User = userOrg
             });
 
             dbContext.Profile.Add(new Profile
             {
                 ProfileId = 2,
                 Name = "Vol Profile",
-                Phone = "987654321",
+                Phone = "9876543210",
                 Description = "Vol Desc",
-                Address = "Vol Address",
+                Address = "456 Vol Ave",
                 City = "Vol City",
-                State = "Vol State",
-                Zip = "11111-111",
-                UserId = userVol.UserId
+                State = "VS",
+                Zip = "98765-432",
+                UserId = userVol.UserId,
+                User = userVol
             });
 
             dbContext.SaveChanges();
         }
-
-
-        [Fact]
-        public async Task GetProfilesOrganization_ReturnsOk_WithProfiles()
-        {
-            var result = await _controller.GetProfilesOrganization();
-
-            var okResult = Assert.IsType<OkObjectResult>(result.Result);
-            var profiles = Assert.IsType<List<Profile>>(okResult.Value);
-
-            Assert.NotEmpty(profiles);
-            Assert.All(profiles, p => Assert.Equal(UserType.Organization, p.User.Type));
-        }
-
 
         [Fact]
         public async Task GetProfileById_ReturnsProfile_WhenExists()
@@ -103,8 +93,11 @@ namespace Solidariza.Tests
             var result = await _controller.GetProfileById(1);
 
             var okResult = Assert.IsType<ActionResult<Profile>>(result);
-            Assert.NotNull(okResult.Value);
-            Assert.Equal(1, okResult.Value.ProfileId);
+            var profile = Assert.IsType<Profile>(okResult.Value);
+            Assert.NotNull(profile);
+            Assert.Equal(1, profile.ProfileId);
+            Assert.Equal("Org Profile", profile.Name);
+            Assert.Equal("1234567890", profile.Phone);
         }
 
         [Fact]
@@ -121,8 +114,11 @@ namespace Solidariza.Tests
             var result = await _controller.GetProfileByUserId(1);
 
             var actionResult = Assert.IsType<ActionResult<Profile>>(result);
-            Assert.NotNull(actionResult.Value);
-            Assert.Equal(1, actionResult.Value.UserId);
+            var profile = Assert.IsType<Profile>(actionResult.Value);
+            Assert.NotNull(profile);
+            Assert.Equal(1, profile.UserId);
+            Assert.Equal("Org Profile", profile.Name);
+            Assert.Equal("1234567890", profile.Phone);
         }
 
         [Fact]
@@ -134,18 +130,41 @@ namespace Solidariza.Tests
         }
 
         [Fact]
+        public async Task GetProfileByUserId_ThrowsException_ReturnsProblem()
+        {
+            var controller = new ProfileController(null!); // Força null para causar exceção
+
+            var result = await controller.GetProfileByUserId(1);
+
+            var problemResult = Assert.IsType<ObjectResult>(result.Result);
+            Assert.Equal(500, problemResult.StatusCode);
+            Assert.IsType<ProblemDetails>(problemResult.Value);
+        }
+
+        [Fact]
         public async Task CreateProfile_ReturnsOk_WithCreatedProfile()
         {
+            var newUser = new User
+            {
+                UserId = 3,
+                Name = "New User",
+                Email = "new@example.com",
+                Password = "pass",
+                Type = UserType.Volunteer
+            };
+            _dbContext.User.Add(newUser);
+            _dbContext.SaveChanges();
+
             var newProfile = new NewProfile
             {
                 Name = "New Profile",
-                Phone = "000000000",
+                Phone = "5555555555",
                 Description = "New Description",
-                Address = "New Address",
+                Address = "789 New St",
                 City = "New City",
-                State = "New State",
-                Zip = "22222-222",
-                UserId = 3,
+                State = "NS",
+                Zip = "55555-555",
+                UserId = 3
             };
 
             var result = await _controller.CreateProfile(newProfile);
@@ -153,6 +172,25 @@ namespace Solidariza.Tests
             var okResult = Assert.IsType<OkObjectResult>(result);
             var createdProfile = Assert.IsType<Profile>(okResult.Value);
             Assert.Equal("New Profile", createdProfile.Name);
+            Assert.Equal("5555555555", createdProfile.Phone);
+            Assert.Equal(3, createdProfile.UserId);
+
+            var dbProfile = await _dbContext.Profile.FirstOrDefaultAsync(p => p.UserId == 3);
+            Assert.NotNull(dbProfile);
+            Assert.Equal("New Profile", dbProfile.Name);
+        }
+
+        [Fact]
+        public async Task CreateProfile_ThrowsException_ReturnsProblem()
+        {
+            var controller = new ProfileController(null!); // Força null para causar exceção
+            var newProfile = new NewProfile();
+
+            var result = await controller.CreateProfile(newProfile);
+
+            var problemResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(500, problemResult.StatusCode);
+            Assert.IsType<ProblemDetails>(problemResult.Value);
         }
 
         [Fact]
@@ -161,11 +199,11 @@ namespace Solidariza.Tests
             var updateProfile = new UpdateProfile
             {
                 Name = "Updated Name",
-                Phone = "555555555",
+                Phone = "5555555555",
                 Description = "Updated Description",
                 Address = "Updated Address",
                 City = "Updated City",
-                State = "Updated State",
+                State = "US",
                 Zip = "33333-333"
             };
 
@@ -174,8 +212,13 @@ namespace Solidariza.Tests
             Assert.IsType<NoContentResult>(result);
 
             var updated = await _dbContext.Profile.FindAsync(1);
+            Assert.NotNull(updated);
             Assert.Equal("Updated Name", updated.Name);
-            Assert.Equal("555555555", updated.Phone);
+            Assert.Equal("5555555555", updated.Phone);
+            Assert.Equal("Updated Address", updated.Address);
+            Assert.Equal("Updated City", updated.City);
+            Assert.Equal("US", updated.State);
+            Assert.Equal("33333-333", updated.Zip);
         }
 
         [Fact]
@@ -183,7 +226,13 @@ namespace Solidariza.Tests
         {
             var updateProfile = new UpdateProfile
             {
-                Name = "Does not matter"
+                Name = "Updated Name",
+                Phone = "5555555555",
+                Description = "Updated Description",
+                Address = "Updated Address",
+                City = "Updated City",
+                State = "US",
+                Zip = "33333-333"
             };
 
             var result = await _controller.PutProfile(999, updateProfile);
@@ -208,6 +257,31 @@ namespace Solidariza.Tests
             var result = await _controller.DeleteProfile(999);
 
             Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetProfilesOrganization_ReturnsOk_WithOrganizationProfiles()
+        {
+            var result = await _controller.GetProfilesOrganization();
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var profiles = Assert.IsType<List<Profile>>(okResult.Value);
+            Assert.NotEmpty(profiles);
+            Assert.All(profiles, p => Assert.Equal(UserType.Organization, p.User.Type));
+            Assert.Single(profiles); // Deve retornar apenas o perfil com UserType.Organization
+            Assert.Equal("Org Profile", profiles[0].Name);
+        }
+
+        [Fact]
+        public async Task GetProfilesOrganization_ThrowsException_ReturnsProblem()
+        {
+            var controller = new ProfileController(null!); // Força null para causar exceção
+
+            var result = await controller.GetProfilesOrganization();
+
+            var problemResult = Assert.IsType<ObjectResult>(result.Result);
+            Assert.Equal(500, problemResult.StatusCode);
+            Assert.IsType<ProblemDetails>(problemResult.Value);
         }
     }
 }
